a5e21963538536f17956ca0b92b129ddc675d774
liam <liamla6@gmail.com>
2022-03-11 21:26:38 -0500

    Ajout des méthodes de recherche dans ClientService

--- src/main/java/al420445/Service/ClientService.java
+++ src/main/java/al420445/Service/ClientService.java
@@ -2,18 +2,36 @@ package al420445.Service;
 
 import al420445.Persistence.LibraryDao;
 import al420445.Persistence.LibraryDaoJPA;
+import al420445.models.Library.Livre;
 
 public class ClientService {
 
     private LibraryDao dao;
 
     public ClientService(LibraryDaoJPA libraryDaoJPA) {
+        this.dao = libraryDaoJPA;
     }
 
     public long createClient(String name, String address) {
         return dao.createClient(name, address);
     }
 
+
+    public Livre getLivreWithTitle(String title){
+        return dao.getLivreWithTitle(title);
+    }
+
+    public Livre getLivreWithAuthor(String author){
+        return dao.getLivreWithAuthor(author);
+    }
+
+    public Livre getLivreWithYear(long year){
+        return dao.getLivreWithYear(year);
+    }
+
+    public Livre getLivreWithGenre(String genre){
+        return dao.getLivreWithGenre(genre);
+    }
 
     public long borrowBook(){
         return 0;
